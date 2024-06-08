9aa9dc812d66fae0ad845aac38503f24ba1b779b
Tatiana Chebotar <57943361+tatacheba@users.noreply.github.com>
2024-06-08 19:45:31 +0200

    relocate App.tsx

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,5 @@
-// pages/index.js
-import React, { useState, useEffect } from "react";
-import BookHeader from "../components/BookHeader";
-import RecipeBook from "../components/RecipeBook";
-import styles from "./_app";
+import App from "../components/App";
 
-const Home = () => {
-    const [recipes, setRecipes] = useState([]);
-    const [currentRecipe, setCurrentRecipe] = useState(null);
-
-    useEffect(() => {
-        // Fetch recipes from the API
-        async function fetchRecipes() {
-            const response = await fetch("/api/recipes");
-            const data = await response.json();
-            setRecipes(data.recipes);
-            if (data.recipes.length > 0) {
-                setCurrentRecipe(data.recipes[0]);
-            }
-        }
-        fetchRecipes();
-    }, []);
-
-    const selectRecipe = (recipe) => {
-        setCurrentRecipe(recipe);
-    };
-
-    return (
-        <div className={styles.container}>
-            <BookHeader
-                recipes={recipes}
-                selectRecipe={selectRecipe}
-                currentRecipe={currentRecipe}
-            />
-            {currentRecipe && (
-                <RecipeBook
-                    recipes={recipes}
-                    currentRecipe={currentRecipe}
-                    selectRecipe={selectRecipe}
-                />
-            )}
-        </div>
-    );
-};
-
-export default Home;
+export default function Home() {
+    return <App />;
+}
