a634a2d09508fa7d7488e55d26470143ffbf8669
Tatiana Chebotar <57943361+tatacheba@users.noreply.github.com>
2024-06-11 10:04:18 +0200

    Updates

---
+++ standalone/pages/_document.tsx
@@ -0,0 +1,24 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Foundation:wght@400..700&family=Mynerve&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href="../public/images/icon.ico"
+                ></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
